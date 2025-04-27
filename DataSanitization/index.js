494192505e30ac834274fbf734944e63c675b030
Sayantan <sayantanhalder43@gmail.com>
2025-04-27 21:37:13 +0530

    First commit

---
+++ DataSanitization/index.js
@@ -0,0 +1,58 @@
+const main = require("./database");
+const { user } = require("./model/user");
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/register", async (req, res) => {
+  const ans = await user.find({});
+  res.send(ans);
+});
+
+app.post("/register", async (req, res) => {
+  try {
+    await user.create(req.body);
+    res.status(200).send("User registered Successfully!!");
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: err.message });
+  }
+});
+
+app.get("/user/:id", async (req, res) => {
+  try {
+    const result = await user.findById(req.params.id);
+    res.send(result);
+  } catch (err) {
+    res.send("Error" + err.message);
+  }
+});
+
+app.delete("/user/:id", async (req, res) => {
+  try {
+    const result = await user.findByIdAndDelete(req.params.id);
+    res.send(result);
+  } catch (err) {
+    res.send("Error" + err.message);
+  }
+});
+
+app.patch("/user/:id", async (req, res) => {
+  try {
+    const result = await user.findByIdAndUpdate(req.params.id);
+    res.send(result);
+  } catch (err) {
+    res.send("Error" + err.message);
+  }
+});
+
+main()
+  .then(() => {
+    console.log("Connected to Database");
+    app.listen(5900, () => {
+      console.log("In 5900 port");
+    });
+  })
+  .catch((err) => console.log(err));
---
+++ DataSanitization/model/user.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const userschema = new Schema({
+  firstname: {
+    type: String,
+    required: true,
+    maxLength: 15,
+  },
+  lastname: {
+    type: String,
+    required: true,
+    maxLength: 15,
+  },
+  age: {
+    type: Number,
+    required: true,
+  },
+  gender: {
+    type: String,
+    required: true,
+    enum: ["male", "female", "others"],
+  },
+  emailid: {
+    type: String,
+    required: true,
+    lowercase: true,
+    trim: true,
+    unique: true, // ✅ only email should be unique
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  photo: {
+    type: String,
+  },
+});
+
+const user = mongoose.model("user", userschema);
+
+module.exports = { user };
